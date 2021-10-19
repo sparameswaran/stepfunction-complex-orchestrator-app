dd482065a6067a200e86f3fc10562539af8a4a07
Sabha Parameswaran <sabhap@amazon.com>
2021-10-18 21:58:07 -0700

    Add Batch Array Job sync submit

--- batch-script/batch-notify-step-function.sh
+++ batch-script/batch-notify-step-function.sh
@@ -16,15 +16,27 @@ STEP_FUNCTION_ID=$4
 STEP_FUNCTION_TASK_ID=$5
 cat > send-message.json <<EOF_OF_MSG
 {
-"Status": "$STEP_FUNCTION_STATUS",
-"ResponseMessage": "$SQS_MSG_BODY",
-"Function": "$STEP_FUNCTION_ID",
-"Task": "$STEP_FUNCTION_TASK_ID"
+    "Status": "$STEP_FUNCTION_STATUS",
+    "ResponseMessage": "$SQS_MSG_BODY",
+    "Function": "$STEP_FUNCTION_ID",
+    "Task": "$STEP_FUNCTION_TASK_ID"
 }
 EOF_OF_MSG
 
 sleep $SLEEP_INT
 date
+
+# If step function id does not start with valid aws arn, set it to empty
+if [[ "$STEP_FUNCTION_ID" != "arn:aws:states"* ]]; then
+  STEP_FUNCTION_ID=""
+fi
+
+# If no step function id or task id specified, just return
+if [ "$STEP_FUNCTION_ID" == "" -o "$STEP_FUNCTION_TASK_ID" == "" ]; then
+  echo "Completed batch job! Nothing to notify, exiting!!"
+  exit 0
+fi
+
 echo "Done with activity, going to notify Step function : $STEP_FUNCTION_ID with task id: $STEP_FUNCTION_TASK_ID"
 check_for_success=$(echo $STEP_FUNCTION_STATUS | grep -i ccess)
 echo $check_for_success
@@ -32,7 +44,7 @@ echo $check_for_success
 if [ -z "$check_for_success" ]; then
 	  aws stepfunctions send-task-failure --task-token $STEP_FUNCTION_TASK_ID --error failed
   else
-	    aws stepfunctions send-task-success --task-token $STEP_FUNCTION_TASK_ID --task-output file://send-message.json
+	  aws stepfunctions send-task-success --task-token $STEP_FUNCTION_TASK_ID --task-output file://send-message.json
 fi
 
 echo "bye bye!!"
